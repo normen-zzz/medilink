8273a82540edb86e30700ecf3afc39b52790d5fb
normen-zzz <normanardian24@gmail.com>
2020-10-07 12:59:36 +0700

    norman update views diagnosa.php
    jsbds

--- program/views/master/diagnosa.php
+++ program/views/master/diagnosa.php
@@ -66,7 +66,7 @@
             <section class="content-header">
                 <h1>
                     Master
-                    <small>TINDAKAN DAN DIAGNOSA</small>
+                    <small>TINDAKAN DAN DIAGNOSAaa</small>
                 </h1>
                 <ol class="breadcrumb">
                     <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
