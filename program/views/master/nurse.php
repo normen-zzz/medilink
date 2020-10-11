4174158ceb665b03818df7b407d0875695046e33
dinarizkifebriyanti <dinarizfeb@gmail.com>
2020-10-11 13:43:17 +0700

    update nurse.php

--- program/views/master/nurse.php
+++ program/views/master/nurse.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-  <?php //$this->load->view('primer/head');?>
+  <?php //$this->load->view('primer/head');
+  ?>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>MEDILINK</title>
@@ -42,155 +44,148 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
 <!-- ADD THE CLASS sidebar-collapse TO HIDE THE SIDEBAR PRIOR TO LOADING THE SITE -->
+
 <body class="hold-transition skin-blue sidebar-collapse sidebar-mini fixed">
-<!-- Site wrapper -->
-<div class="wrapper">
+  <!-- Site wrapper -->
+  <div class="wrapper">
 
-   <?php $this->load->view('primer/header');?>
-
-
-  <!-- =============================================== -->
-
-  <!-- Left side column. contains the sidebar -->
-   <?php $this->load->view('primer/left_side');?>
+    <?php $this->load->view('primer/header'); ?>
 
 
-  <!-- =============================================== -->
+    <!-- =============================================== -->
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-       MASTER
-        <small>Rawat inap</small>
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li><a href="#">Halaman</a></li>
-        <li class="active">Halaman Sekarang</li>
-      </ol>
-    </section>
+    <!-- Left side column. contains the sidebar -->
+    <?php $this->load->view('primer/left_side'); ?>
 
-    <!-- Main content -->
-    <section class="content">
-    
-      <!-- Default box -->
-	  
-      <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">Nurse Station</h3>
 
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
+    <!-- =============================================== -->
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <h1>
+          MASTER
+          <small>Rawat inap</small>
+        </h1>
+        <ol class="breadcrumb">
+          <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+          <li><a href="#">Halaman</a></li>
+          <li class="active">Halaman Sekarang</li>
+        </ol>
+      </section>
+
+      <!-- Main content -->
+      <section class="content">
+
+        <!-- Default box -->
+
+        <div class="box">
+          <div class="box-header with-border">
+            <h3 class="box-title">Nurse Station</h3>
+
+            <div class="box-tools pull-right">
+              <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
+                <i class="fa fa-minus"></i></button>
+              <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+                <i class="fa fa-times"></i></button>
+            </div>
           </div>
-        </div>
-        <div class="box-body">
-                        <form class="form-horizontal">
-                            <div class="box-body">
-                                <div class="form-group">
-                                    <label for="inputEmail3" class="col-sm-1">ID</label>
+          <div class="box-body">
+            <div class="margin">
+              <button type="button" class="btn btn-success" data-toggle="modal" data-target="#modal-default">
+                Tambah Paviliun
+              </button>
+            </div>
 
-                                    <div class="col-sm-1">
-                                        <input type="email" class="form-control" id="inputEmail3" placeholder="ID">
-                                    </div>
-                                    
-                                </div>
-                                <div class="form-group">
-                                    <label for="inputPassword3" class="col-sm-1">Paviliun</label>
+          </div>
+          <!-- /.box-body -->
 
-                                    <div class="col-sm-4">
-                                        <input type="text" class="form-control" id="inputEmail3" placeholder="Paviliun">
-                                    </div>
-                                  </div>
-                                </div>
-                              </form>
-                
-              </div>
-              <!-- /.box-body -->
-              
 
-         <div class="box">
-            
+          <div class="box">
+
             <!-- /.box-header -->
             <div class="box-body">
               <table id="example1" class="table table-bordered table-striped">
                 <tr>
                   <th style="width: 100px;">ID</th>
                   <th>Nama Paviliun</th>
-                  
+
                 </tr>
                 <tr>
-                  <td>1.</td>
+                  <td>1</td>
                   <td>RPA/RPM</td>
-                  
+
                 </tr>
                 <tr>
-                  <td>2.</td>
+                  <td>2</td>
                   <td>RPU DEWASA</td>
-                 
+
                 </tr>
                 <tr>
-                  <td>3.</td>
+                  <td>3</td>
                   <td>HCU</td>
-                  
+
                 </tr>
                 <tr>
-                  <td>4.</td>
-				  <td>ICU</td>
-                  
+                  <td>4</td>
+                  <td>ICU</td>
+
                 </tr>
               </table>
             </div>
             <!-- /.box-body -->
-            <div class="box-footer clearfix">
-              <ul class="pagination pagination-sm no-margin pull-right">
-                <li><a href="#">&laquo;</a></li>
-                <li><a href="#">1</a></li>
-                <li><a href="#">2</a></li>
-                <li><a href="#">3</a></li>
-                <li><a href="#">&raquo;</a></li>
-              </ul>
-            </div>
-          </div>
-          <!-- /.box -->
-		  
-		          <div class="box-footer">
-                <button type="submit" class="btn btn-success mb-3">Export Excel</button>
-                <button type="reset" class="btn btn-danger pull-right">Batal</button>
-                <button type="submit" class="btn btn-info pull-right">Simpan</button>
+            <div class="modal fade" id="modal-default">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title">Tambah Paviliun</h4>
+                  </div>
+                  <div class="modal-body">
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">ID</label>
+                      <input type="text" class="form-control" id="exampleInputEmail1" placeholder="0">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Nama Paviliun</label>
+                      <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Nama Paviliun">
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button> 
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
+            </div>
 
-          
-                
-                
+              <!-- /.box -->
 
-          
-        <!--<div class="box-footer">
+              <!--<div class="box-footer">
           Footer
         </div>
         <!-- /.box-footer-->
-		
 
-      </div>
-      <!-- /.box -->
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
 
-  <?php $this->load->view('primer/footer');?>
+            </div>
+            <!-- /.box -->
+      </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
 
- 
-  <!-- Add the sidebar's background. This div must be placed
+    <?php $this->load->view('primer/footer'); ?>
+
+
+    <!-- Add the sidebar's background. This div must be placed
        immediately after the control sidebar -->
-  <div class="control-sidebar-bg"></div>
-</div>
-<!-- ./wrapper -->
+    <div class="control-sidebar-bg"></div>
+  </div>
+  <!-- ./wrapper -->
 
-<?php $this->load->view('primer/jse');?>
+  <?php $this->load->view('primer/jse'); ?>
 </body>
+
 </html>
